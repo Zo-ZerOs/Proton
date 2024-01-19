f2680fda5eb56b5844489ac8a4a0f2045be801f5
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2024-01-19 18:57:41 +0700

    Create 19-01-2024.c

---
+++ 19-01-2024.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    printf("- ASCII -\n");
+    char c = 'A';
+    // จำแค่ 3 ตัวก็พอ
+    // ascii: '0'=48, 'A'=65, 'a'=97
+    printf("%d", c);
+    
+    char a[100] = "Hello"; //array of character = string
+    for (int i = 0 ; i < 5 ; i++) {
+        printf("%c", a[i]);
+    }
+    printf("\n");
+    
+    printf("%s", a); //สำหรับ string เท่านั้น
+    
+    char b[100] = "HelloWorld";
+    for (int i = 0 ; i < 10 ; i++) {
+        printf("%c", b[i] + 1);
+    }
+
+    char c = 'a'; //'a' = 97
+    if (c < 98) printf("Yes"); //จะปริ้น Yes เพราะ 'a' < 98
+    else printf("No");
+
+    return 0;
+}
+
+
+
+
